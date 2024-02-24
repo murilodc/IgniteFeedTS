d8822aa13633437713b5ccf79a8e599dc0adbd7b
Murilo De Carli <muriloodecarli@gmail.com>
2024-02-23 21:41:57 -0300

    Ignite feed with ts

---
+++ src/App.tsx
@@ -0,0 +1,64 @@
+import { Header } from "./components/Header";
+import { Post, PostType } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from "./App.module.css";
+
+import "./global.css";
+
+const posts: PostType[] = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/murilodc.png",
+      name: "Murilo De Carli",
+      role: "Desenvolvedor De Software",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2024-02-04 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/maykbrito.png",
+      name: "Quilian Moretti",
+      role: "Digital Influencer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2024-02-03 20:00:00"),
+  },
+];
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map((post) => {
+            return <Post key={post.id} post={post} />;
+          })}
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
